3397636ff5268e359bc60878a4e4c89173840df0
Muskan LNU <muskanlnu@microsoft.com>
2024-03-06 13:06:57 +0530

    initial commit with frontend and backend

---
+++ backend/src/main/java/com/ms/cloudblockers/CloudblockersApplication.java
@@ -0,0 +1,13 @@
+package com.ms.cloudblockers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudblockersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudblockersApplication.class, args);
+	}
+
+}
